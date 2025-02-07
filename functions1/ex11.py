f7e2dd5ec1ec403dfaf781ba735af40c511c76a5
OlzhasDuisenbay <o_duisenbay@kbtu.kz>
2025-02-07 20:23:49 +0500

    Create ex11.py

---
+++ functions1/ex11.py
@@ -0,0 +1,9 @@
+"""Write a Python function that checks whether a word or phrase is or not.
+Note: A palindrome is word, phrase, or sequence that reads the same backward
+as forward, e.g., madampalindrome"""
+def is_palindrome(word):
+    return word == word[::-1]
+
+word = input("Enter a word or phrase: ").replace(" ", "")
+
+print(f"The word or phrase '{word}' is {'a palindrome' if is_palindrome(word) else 'not a palindrome'}.")
