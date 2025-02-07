729ac4a807ca42e3f8268c6c017359abbe802031
OlzhasDuisenbay <o_duisenbay@kbtu.kz>
2025-02-07 23:32:23 +0500

    Create ex3.py

---
+++ functions2/ex3.py
@@ -0,0 +1,19 @@
+"""Write a function that takes a category name and
+returns just those moviesunder that category"""
+def movies_by_category(movies, category):
+    result = []
+    for movie in movies:
+        if movie["category"].lower() == category.lower():
+            result.append(movie)
+    return result
+
+movies = [
+    {"name": "Hitman", "imdb": 6.3, "category": "Action"},
+    {"name": "Dark Knight", "imdb": 9.0, "category": "Adventure"},
+    {"name": "The Choice", "imdb": 6.2, "category": "Romance"},
+    {"name": "Colonia", "imdb": 7.4, "category": "Romance"},
+]
+
+print(movies_by_category(movies, "Romance"))
+""""""[{'name': 'The Choice', 'imdb': 6.2, 'category': 'Romance'}, {'name': 'Colonia', 'imdb': 7.4, 'category': 'Romance'}]"""
+
