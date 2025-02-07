04b7dc8cf700cc102dd96ae05e1e7bdad5ec4424
OlzhasDuisenbay <o_duisenbay@kbtu.kz>
2025-02-07 20:22:16 +0500

    Create ex10.py

---
+++ functions1/ex10.py
@@ -0,0 +1,13 @@
+"""Write a Python function that takes a list and returns a new list with unique
+elements of the first list. Note: don't use collection .set"""
+def unique_elements(lst):
+    unique_lst = []
+    for element in lst:
+        if element not in unique_lst:
+            unique_lst.append(element)
+    return unique_lst
+
+original_list = [1, 2, 2, 3, 3, 3, 4, 5, 5, 6]
+new_list = unique_elements(original_list)
+print(f"Original list: {original_list}")
+print(f"List with unique elements: {new_list}")
